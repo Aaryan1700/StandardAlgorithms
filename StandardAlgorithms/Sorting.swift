c5f8f9b15398c2baf85a34627e67a2fd8be10571
Aaryan Agnihotri <agnihotri.aaryan@etoncollege.org.uk>
2024-11-15 17:09:39 +0000

    start sorting algos

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,14 @@
+//
+//  Sorting.swift
+//  StandardAlgorithms
+//
+//  Created by Agnihotri, Aaryan (Coll) on 15/11/2024.
+//
+
+import Foundation
+
+class Sorting{
+    
+    func quickSorted()
+    
+}
